669a9fff92e69f03edd6de8c2af03f9c40cf9a35
vupt1 <vupt1@smartosc.com>
2013-10-08 15:12:12 +0700

    commit

---
+++ app/code/local/SEA/Setup/data/sea_setup_setup/data-upgrade-0.1.9-0.2.0.php
@@ -0,0 +1,52 @@
+<?php
+try {
+
+    $installer = $this;
+
+    $installer->startSetup();
+
+    $content = <<<EOD
+ <script type="text/javascript">// <![CDATA[
+    jQuery(document).ready(function(){
+	jQuery('.bxslider').bxSlider({
+	  auto: true,
+	  autoControls: true
+	});
+    });
+// ]]></script>
+<ul class="bxslider">
+<li><img src="http://www.verbenaproducts.com/media/wysiwyg/home-banner/background-slider1.jpg" alt="" /></li>
+<li><img src="http://www.verbenaproducts.com/media/wysiwyg/home-banner/background-slider2.jpg" alt="" /></li>
+<li><img src="http://www.verbenaproducts.com/media/wysiwyg/home-banner/background-slider3.jpg" alt="" /></li>
+<li><img class="de1" style="margin: 0px; padding: 0px; color: #638ca8;" src="http://www.verbenaproducts.com/media/wysiwyg/home-banner/background-slider3.jpg" alt="" /></li>
+</ul>
+<div class="product">
+<h1>Featured Products</h1>
+<a id="feature-slide-prev2" class="prev" href="javascript:void(0)">&lt;</a> <a id="feature-slide-next2" class="next" href="javascript:void(0)">&gt;</a>
+<div class="peatured-products">{{block type="catalog/product_list" category_id="9" template="catalog/product/featured.phtml"}}</div>
+<div class="clearfix">&nbsp;</div>
+<div class="peatured-big-products">{{block type="catalog/product_list" category_id="10" template="catalog/product/featured_big.phtml"}}</div>
+</div>
+EOD;
+    $cmsPage = array(
+        'title' => 'Home page',
+        'identifier' => 'home',
+        'content' => $content,
+        'is_active' => 1,
+        'stores' => array(0),
+        'root_template' => 'one_column'
+    );
+    $privacy = Mage::getModel('cms/page')->load('home');
+    if(!$privacy->getId()){
+        Mage::getModel('cms/page')->setData($cmsPage)->save();
+    }else{
+        $privacy->setContent($content)->setRootTemplate('one_column')->save();
+    }
+
+
+    $installer->endSetup();
+
+} catch (Excpetion $e) {
+    Mage::logException($e);
+    Mage::log("ERROR IN SETUP " . $e->getMessage());
+}
